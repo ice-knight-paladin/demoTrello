43637bdef0e4521e07875c7c026fcb30ddffbc36
Arseniy <oav22mk309@student.bmstu.ru>
2024-10-16 21:51:35 +0300

    try get projects and task_state

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import fetch_data
+from .views import project_task_states
 
 urlpatterns = [
-    path('fetch/', fetch_data, name='fetch_data'),
+    path("project/", fetch_data, name='fetch_data'),
+    path('projects/<str:project_id>/task-states/', project_task_states, name='task_states'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 
 def fetch_data(request):
-
     url = 'http://localhost:8080/api/projects'
     response = requests.get(url)
 
@@ -15,5 +14,22 @@ def fetch_data(request):
     context = {
         'data': data
     }
+    return render(request, "C:\\Users\\Arseniy\\PycharmProjects\\demoTrello\\myapp\\templates\\project.html", context)
 
-    return render(request, "C:\\Users\\Arseniy\\PycharmProjects\\demoTrello\\myapp\\templates\\data.html", context)
+
+def project_task_states(request, project_id):
+    # TODO project_name
+    project_name = request.GET.get('project_id')
+    project_name = "Project1"
+    print(project_id)
+    # TODO http://localhost:8080/api/projects/{project_id}/task-states
+    task_states_response = requests.get(f'http://localhost:8080/api/projects/102/task-states')
+    task_states_data = task_states_response.json()
+    print(task_states_data)
+
+    return render(request,
+                  "C:\\Users\\Arseniy\\PycharmProjects\\demoTrello\\myapp\\templates\\task_state.html",
+                  {
+                      'project_name': project_name,
+                      'task_states': task_states_data
+                  })
